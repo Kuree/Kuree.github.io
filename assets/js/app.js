7760c3aa62b234ba35c97e3b42f7c96521792c96
Keyi <keyi@stanford.edu>
2018-08-08 20:15:53 -0700

    minor changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -217,7 +217,7 @@ new Vue({
         "active_path": "/",
         "profile": {
             "name": "Keyi Zhang",
-            "desc": "I am a PhD student in Computer Science at Stanford University. My undergrad was at Bucknell University, where I did independent researches under Dr. Alan Marchiori. Go Bison! Besides computer science, I enjoy playing video games and hanging out with friends.",
+             "desc": "I am a PhD student in Computer Science at Stanford University, advised by Mark Horowitz. I am currently working on place and route tools for CGRA. I received a B.S. in Computer Science and Engineering from Bucknell University. Besides computer science, I enjoy playing video games and hanging out with friends.",
             "profile_desc": "Picture with Donald Knuth.",
             "email_user": "hi",
             "email_domain": "keyizhang.com",
